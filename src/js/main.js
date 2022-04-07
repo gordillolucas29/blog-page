c9891886e7a7144b6c65d1f3fb85de3110af6125
gordillolucas29 <gordillolucas29@gmail.com]>
2022-04-07 05:44:45 -0300

    add changes

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ class Header extends HTMLElement {
         <!--NAVIGATION-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
             <div class="container-fluid mx-5">
-                <a class="navbar-brand" href="/">Surname</a>
+                <a class="navbar-brand" href="/">Gordillo</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                     aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
